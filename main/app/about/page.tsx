57ac78b34a74def539140a4da3168ad14e3f823f
Luke Penner <lukepenner@hotmail.ca>
2023-09-11 20:33:05 -0700

    Error, loading, metadata

--- main/app/about/page.tsx
+++ main/app/about/page.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link"
+import type {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: 'About',
+  description: 'About page'
+}
 
 export default function About() {
+  //throw new Error('whoopsy')
   return (
     <>
     <h1>About</h1>
--- main/app/page.tsx
+++ main/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
+import type {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Home page'
+}
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <h1>This is the Main Page</h1>
       <Link href="/about">Link to about page</Link>
     </main>
