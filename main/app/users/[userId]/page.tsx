31941e5110cf2e7303a3c0f87642d03ed849a5c5
Luke Penner <lukepenner@hotmail.ca>
2023-09-14 22:16:02 -0700

    Added User data fetching and display

--- main/app/users/[userId]/page.tsx
+++ main/app/users/[userId]/page.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import getUser from '@/lib/getUser'
+import getUserPosts from '@/lib/getUserPosts'
+import {Suspense} from 'react'
+import UserPosts from './components/UserPosts'
+import Link from 'next/link'
 type Params = {
   params: {
     userId: string
   }
 }
 
-export default function UserPage({ params: {userId} }: Params) {
+export default async function UserPage({ params: {userId} }: Params) {
+  const userData: Promise<User> = getUser(userId)
+  const userPostsData: Promise<Post[]> = getUserPosts(userId)
+  
+  //const [user, userPosts] = await Promise.all([userData, userPostsData])
+  
+  const user = await userData
+
   return (
-    <div>UserP</div>
+    <>
+      <Link href='/users'>Back to Users</Link>
+      <h2>{user.name}</h2>
+      <br />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+    </>
   )
 }
