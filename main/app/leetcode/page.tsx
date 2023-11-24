bf77102ab0522027b0abf5b517d3566cc365d504
Luke Penner <lukepenner@hotmail.ca>
2023-11-23 21:36:15 -0800

    codeblock working

--- main/app/leetcode/page.tsx
+++ main/app/leetcode/page.tsx
@@ -1,28 +1,28 @@
+"use client";
 import React from 'react'
 import { CopyBlock,dracula } from "react-code-blocks";
 
+
+
 export default function Leetcode() {
-  let codeBlock1 = `var twoSum = function(nums, target) {
-    let firstNum = null;
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] <= target){
-            firstNum = nums[i];
-            console.log("firstNum: " + firstNum)
-            for(let j = i+1; j < nums.length; j++){
-                console.log("j: " + j)
-                if(nums[i] + nums[j] === target){
-                    return([i,j]);
-                }
-            }
-        }
-    }
-    return([]);
-    testing
-};`
+  function codeBlock1 (code: string, language: string, showLineNumbers: boolean) {
+    return (
+      <CopyBlock
+        text={code}
+        language={language}
+        showLineNumbers={showLineNumbers}
+      />
+    );
+  }
   return (
     <>
     <div>This is the first Leetcode Problem I've done, Two Sum</div>
-    {codeBlock1}
+    {codeBlock1(
+`testing
+test2
+test3`, 
+      'javascript',
+      true)}
     </>
   )
 }
