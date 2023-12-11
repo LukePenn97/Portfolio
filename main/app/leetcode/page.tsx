344ddbd763c55a6b8d48ce224dfacec7998bd44e
Luke Penner <lukepenner@hotmail.ca>
2023-12-10 21:41:23 -0800

    Two sum page formatting and spellcheck

--- main/app/leetcode/page.tsx
+++ main/app/leetcode/page.tsx
@@ -17,7 +17,7 @@ export default function Leetcode() {
   }
   return (
     <>
-    <div>This is the first Leetcode Problem I've done, Two Sum </div>
+    <h1>Two Sum </h1>
     <br/>
     <div>Instructions: </div>
     <br/>
@@ -33,8 +33,7 @@ export default function Leetcode() {
     <br/>
     <div>First try:</div>
     <br/>
-    {codeBlock1(`
-/**
+    {codeBlock1(`/**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
@@ -61,20 +60,19 @@ var twoSum = function(nums, target) {
       <br/>
       <div>I translated the code example on leetcode from java to javascript:</div>
       <br/>
-      {codeBlock1(`
-      var twoSum = function(nums, target) {
-        const hashMap = new Map();
-        let n = nums.length;
+      {codeBlock1(`var twoSum = function(nums, target) {
+  const hashMap = new Map();
+  let n = nums.length;
     
-        for(i=0; i < n; i++) {
-            let complement = target - nums[i];
-            if (hashMap.has(complement)){
-                return [hashMap.get(complement), i];
-            }
-            hashMap.set(nums[i], i);
-        }
-        return [];
-    };`,
+  for(i=0; i < n; i++) {
+    let complement = target - nums[i];
+      if (hashMap.has(complement)){
+        return [hashMap.get(complement), i];
+      }
+    hashMap.set(nums[i], i);
+  }
+  return [];
+};`,
     'javascript',
     true)}
     <br/>
@@ -82,13 +80,13 @@ var twoSum = function(nums, target) {
     <br/>
     <div>A hashmap takes a value or series of values, they could be strings or numbers, and applies a function to turn them into unique keys.</div>
     <div>These keys value pairs are then mapped to an array, and with a perferct hash function, each key should get a unique spot in the array</div>
-    <div>The best hash function depends on the data you're trying to hash. For example, a simple hash function would be tyo just take the first character of a string and multiply it by the last character's value</div>
+    <div>The best hash function depends on the data you're trying to hash. For example, a simple hash function would be to just take the first character of a string and multiply it by the last character's value</div>
     <div>However, if your dataset has multiple strings that have the same first and last letter, those would all be allocated to the same spot in the array.</div>
     <br/>
     <div>This would slow down the hash map because now you will have to check each array position, or bucket, for multiple entries, or collisions.</div>
-    <div>The code above uses javascript's default hashmap. First we generate a new empty map, then we initiate a loop and set the complement to the target - the current numer in the array</div>
+    <div>The code above uses javascript's default hashmap. First we generate a new empty map, then we initiate a loop and set the complement to the target - the current number in the array</div>
     <div>We then check if the hashmap contains the complement. The map is empty on the first pass, so it skips to the next step.</div>
-    <div>We now insert the first number in the array into the hashmap withh it's index. Then we repeat this process.</div>
+    <div>We now insert the first number in the array into the hashmap with it's index. Then we repeat this process.</div>
     <div>We are only using one loop to iterate through the array, and since hash table lookups take constant time on average, this solution has a time complexity of {"O(n)"}</div>
     <br/>
     <br/>
