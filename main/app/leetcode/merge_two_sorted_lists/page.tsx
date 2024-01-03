17f707bff658b9317dc2c14eb474cb77bc4d0808
Luke Penner <lukepenner@hotmail.ca>
2024-01-02 22:21:53 -0800

    merge two sorted lists

--- main/app/leetcode/merge_two_sorted_lists/page.tsx
+++ main/app/leetcode/merge_two_sorted_lists/page.tsx
@@ -16,7 +16,8 @@ export default function merge_two_sorted_lists() {
   return (
     <>
     <h1>Merge Two Sorted Lists</h1>
-
+    <br/>
+    <div>Honestly I forgot how to do linked lists, here's my first attempt but it didn't really make any sense so I went straight to the solution after this</div>
     <br/>
     {codeBlock1(`/**
  * Definition for singly-linked list.
@@ -30,14 +31,58 @@ export default function merge_two_sorted_lists() {
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    
+const mergeTwoLists = function(list1, list2) {
+    let tempNode = list1
+    while (list1 != null){
+        if (list1.val >= list2.val) {
+            tempNode.val = list1.val
+        }
+        if (list1.val < list2.val) {
+            tempNode.val = list2.val
+        }
+        console.log("list1.val: " + list1.val)
+        console.log("list2.val: " + list2.val)
+        console.log("tempNode.val: " + tempNode.val)
+        list1 = list1.next
+        list2 = list2.next
+        tempNode = tempNode.next
+    }
 };`, 
       'javascript',
       true)}
       <br/>
-      
+      <div>Things I missed are creating an empty listNode (I forgot you can do that), checking that both list nodes exist, and adding a pointer to the temp list</div>
       <br/>
+      {codeBlock1(`var mergeTwoLists = function(list1, list2) {
+    let ans = new ListNode();
+    let ptr = ans;
+    while(list1 && list2){
+        if(list1.val <= list2.val){
+            ans.next = new ListNode(list1.val);
+            list1 = list1.next;
+        }
+        else{
+            ans.next = new ListNode(list2.val);
+            list2 = list2.next;
+        }
+        ans = ans.next;
+    }
+    while(list1){
+        ans.next = new ListNode(list1.val);
+        list1 = list1.next;
+        ans = ans.next;
+    }
+    while(list2){
+        ans.next = new ListNode(list2.val);
+        list2 = list2.next;
+        ans = ans.next;
+    }
+    return ptr.next;
+};`, 
+      'javascript',
+      true)}
+      <br/>
+      <div>I kind of cheated on this one and I'd like to come back to it, but I'll count it for now</div>
     </>
   )
 }
